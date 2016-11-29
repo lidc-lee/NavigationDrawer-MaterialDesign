4bc5bae5d3bd937849d1614dc038eb335ebc4d5e
AA-ldc <1499117534@qq.com>
2016-11-29 10:56:12 +0800

    second commit

--- demo/src/main/java/br/liveo/ndrawer/ui/activity/SecondActivity.java
+++ demo/src/main/java/br/liveo/ndrawer/ui/activity/SecondActivity.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2015 Rudson Lima
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package br.liveo.ndrawer.ui.activity;
 
 import android.content.Intent;
@@ -21,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import br.liveo.interfaces.OnItemClickListener;
@@ -31,23 +17,25 @@ import br.liveo.ndrawer.R;
 import br.liveo.ndrawer.ui.fragment.MainFragment;
 import br.liveo.ndrawer.ui.fragment.ViewPagerFragment;
 
-public class MainActivity extends NavigationLiveo implements OnItemClickListener {
+/**
+ * Created by AA on 2016/11/29.
+ */
+
+public class SecondActivity extends NavigationLiveo implements OnItemClickListener {
 
     private HelpLiveo mHelpLiveo;
 
     @Override
     public void onInt(Bundle savedInstanceState) {
-
         // User Information
-        this.userName.setText("Rudson Lima");
-        this.userEmail.setText("rudsonlive@gmail.com");
-        this.userPhoto.setImageResource(R.drawable.ic_rudsonlive);
-        this.userBackground.setImageResource(R.drawable.ic_user_background_first);
-
+        this.userName.setText("Li Dongchan");
+        this.userEmail.setText("1499117534@qq.com");
+        this.userPhoto.setImageResource(R.drawable.ic_user);
+        this.userBackground.setImageResource(R.drawable.ic_user_background_second);
 
         // Creating items navigation
         mHelpLiveo = new HelpLiveo();
-        mHelpLiveo.add(getString(R.string.inbox), R.drawable.ic_inbox_black_24dp, 7);
+        mHelpLiveo.add(getString(R.string.inbox), R.drawable.ic_inbox_black_24dp, 6);
         mHelpLiveo.addSubHeader(getString(R.string.categories)); //Item subHeader
         mHelpLiveo.add(getString(R.string.starred), R.drawable.ic_star_black_24dp);
         mHelpLiveo.add(getString(R.string.sent_mail), R.drawable.ic_send_black_24dp);
@@ -56,50 +44,50 @@ public class MainActivity extends NavigationLiveo implements OnItemClickListener
         mHelpLiveo.add(getString(R.string.trash), R.drawable.ic_delete_black_24dp);
         mHelpLiveo.add(getString(R.string.spam), R.drawable.ic_report_black_24dp, 120);
 
-        //{optional} - Header Customization - method customHeader
-        //View mCustomHeader = getLayoutInflater().inflate(R.layout.custom_header_user, this.getListView(), false);
-        //ImageView imageView = (ImageView) mCustomHeader.findViewById(R.id.imageView);
+        //{optional} - 自定义头视图
+//        View mCustomHeader = getLayoutInflater().inflate(R.layout.custom_header_user, this.getListView(), false);
+//        ImageView imageView = (ImageView) mCustomHeader.findViewById(R.id.imageView);
 
         with(this).startingPosition(2) //Starting position in the list
                 .addAllHelpItem(mHelpLiveo.getHelp())
                 //{optional} - List Customization "If you remove these methods and the list will take his white standard color"
-                //.selectorCheck(R.drawable.selector_check) //Inform the background of the selected item color
-                //.colorItemDefault(R.color.nliveo_blue_colorPrimary) //Inform the standard color name, icon and counter
+                .selectorCheck(R.drawable.selector_check) //Inform the background of the selected item color
+                .colorItemDefault(R.color.nliveo_blue_colorPrimary) //Inform the standard color name, icon and counter
                 .colorItemSelected(R.color.nliveo_blue_colorPrimary) //State the name of the color, icon and meter when it is selected
-                //.backgroundList(R.color.nliveo_black_light) //Inform the list of background color
-                //.colorLineSeparator(R.color.nliveo_transparent) //Inform the color of the subheader line
+                .backgroundList(R.color.nliveo_black_light) //Inform the list of background color
+                .colorLineSeparator(R.color.nliveo_transparent) //Inform the color of the subheader line
 
                 //{optional} - SubHeader Customization
-                //.colorNameSubHeader(R.color.nliveo_blue_colorPrimary)
-                //.colorLineSeparator(R.color.nliveo_green_colorPrimaryDark)
+                .colorNameSubHeader(R.color.nliveo_blue_colorPrimary)
+                .colorLineSeparator(R.color.nliveo_green_colorPrimaryDark)
 
                 //.removeFooter()
                 .footerItem(R.string.settings, R.drawable.ic_settings_black_24dp)
 
                 //{optional} - Second footer
-                //.footerSecondItem(R.string.settings, R.drawable.ic_settings_black_24dp)
+//                .footerSecondItem(R.string.settings, R.drawable.ic_settings_black_24dp)
 
                 //{optional} - Header Customization
-                //.customHeader(mCustomHeader)
+//                .customHeader(mCustomHeader)
 
                 //{optional} - Footer Customization
-                //.footerNameColor(R.color.nliveo_blue_colorPrimary)
-                //.footerIconColor(R.color.nliveo_blue_colorPrimary)
+                .footerNameColor(R.color.nliveo_blue_colorPrimary)
+                .footerIconColor(R.color.nliveo_blue_colorPrimary)
 
-                //.footerSecondNameColor(R.color.nliveo_blue_colorPrimary)
-                //.footerSecondIconColor(R.color.nliveo_blue_colorPrimary)
+//                .footerSecondNameColor(R.color.nliveo_blue_colorPrimary)
+//                .footerSecondIconColor(R.color.nliveo_blue_colorPrimary)
 
-                //.footerBackground(R.color.nliveo_white)
+                .footerBackground(R.color.nliveo_white)
 
                 //{optional} - Remove color filter icon
-                //.removeColorFilter()
+//                .removeColorFilter()
 
                 .setOnClickUser(onClickPhoto)
                 .setOnPrepareOptionsMenu(onPrepare)
                 .setOnClickFooter(onClickFooter)
 
                 //{optional} - Second footer
-                //.setOnClickFooterSecond(onClickFooter)
+//                .setOnClickFooterSecond(onClickFooter)
                 .build();
 
         int position = this.getCurrentPosition();
@@ -128,26 +116,26 @@ public class MainActivity extends NavigationLiveo implements OnItemClickListener
         setElevationToolBar(position != 2 ? 15 : 0);
     }
 
+    private View.OnClickListener onClickPhoto = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(getApplicationContext(), "onClickPhoto :D", Toast.LENGTH_SHORT).show();
+            closeDrawer();
+        }
+    };
     private OnPrepareOptionsMenuLiveo onPrepare = new OnPrepareOptionsMenuLiveo() {
         @Override
         public void onPrepareOptionsMenu(Menu menu, int position, boolean visible) {
-        }
-    };
-
-    private View.OnClickListener onClickPhoto = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Toast.makeText(getApplicationContext(), "onClickPhoto :D", Toast.LENGTH_SHORT).show();
-            closeDrawer();
+            Toast.makeText(getApplicationContext(), "OnPrepareOptionsMenuLiveo :D", Toast.LENGTH_SHORT).show();
         }
     };
 
     private View.OnClickListener onClickFooter = new View.OnClickListener() {
         @Override
-        public void onClick(View v) {
-            startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
+        public void onClick(View view) {
+            Toast.makeText(getApplicationContext(), "onClickFooter :D", Toast.LENGTH_SHORT).show();
             closeDrawer();
         }
     };
-}
 
+}
